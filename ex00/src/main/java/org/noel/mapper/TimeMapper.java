8186ec422d5da90944d444a5bf72b5ca68deadb9
kimhy <k.hyejin1030@gmail.com>
2023-10-31 13:01:52 +0900

    Mapper XML 설정

--- ex00/src/main/java/org/noel/mapper/TimeMapper.java
+++ ex00/src/main/java/org/noel/mapper/TimeMapper.java
@@ -2,9 +2,14 @@ package org.noel.mapper;
 
 import org.apache.ibatis.annotations.Select;
 
+// org.noel.mapper.TimeMapper.getTime2 -> 
+
 public interface TimeMapper {
 	
+	// ; 없어야 한다
 	@Select("SELECT sysdate From dual")
 	String getTime();
+	
+	String getTime2();
 
 }
