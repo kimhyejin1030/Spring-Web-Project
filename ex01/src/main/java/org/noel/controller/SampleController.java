68513244ca21cb5566058e763e2b5426efbbb6cf
kimhy <k.hyejin1030@gmail.com>
2023-11-06 21:54:34 +0900

    Controller의 리턴 타입

--- ex01/src/main/java/org/noel/controller/SampleController.java
+++ ex01/src/main/java/org/noel/controller/SampleController.java
@@ -8,6 +8,9 @@ import org.noel.domain.SampleDTO;
 import org.noel.domain.SampleDTOList;
 import org.noel.domain.TodoDTO;
 import org.springframework.beans.propertyeditors.CustomDateEditor;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -16,6 +19,7 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import lombok.extern.log4j.Log4j;
 
@@ -110,6 +114,48 @@ public class SampleController {
 		return "/sample/ex04";
 	}
 	
+	@GetMapping("/ex05")
+	public void ex05() {
+		log.info("/ex05........");
+	}
+	
+	@GetMapping("/ex06")
+	public @ResponseBody SampleDTO ex06() {
+		log.info("/ex06..........");
+
+		SampleDTO dto = new SampleDTO();
+		dto.setAge(10);
+		dto.setName("홍길동");
+
+		return dto;
+	}
+	
+	@GetMapping("/ex07")
+	public ResponseEntity<String> ex07() {
+		log.info("/ex07..........");
+
+		// {"name": "홍길동"}
+		String msg = "{\"name\": \"홍길동\"}";
+
+		HttpHeaders header = new HttpHeaders();
+		header.add("Content-Type", "application/json;charset=UTF-8");
+
+		return new ResponseEntity<>(msg, header, HttpStatus.OK);
+	}
+	
+	// Controller의 리턴 타입
+	@GetMapping("/re1")
+	public String re1() {
+		log.info("rea1................");
+		
+		// response.sendRedirect("...");
+		return "redirect:/sample/re2";
+	}
+	
+	@GetMapping("/re2")
+	public void re2() {
+		log.info("rea1................");
+	}
 	
 
 	
